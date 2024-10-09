e87f2cab14f259c16af00060d1c0cf9f9f80611b
Junk-debug <maks.naumovich@gmail.com>
2024-10-09 14:36:09 +0200

    init: init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "@/styles.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Willkommen zur Musik-App</h1>
+    </div>
+  );
+};
+
+export default App;
