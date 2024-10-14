81663beb2205540da25e2192e930fb7abcd0b888
Junk-debug <maks.naumovich@gmail.com>
2024-10-14 22:02:08 +0200

    feat: add proxy middleware to get songs

--- next.config.js
+++ next.config.js
@@ -2,6 +2,23 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+        port: "",
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/proxy/mp3/:filename",
+        destination: "/api/proxy/mp3/:filename",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
