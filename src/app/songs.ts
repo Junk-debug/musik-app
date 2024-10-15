11c368b4939c967fe87810e74b5d17d34ab26bc1
Junk-debug <maks.naumovich@gmail.com>
2024-10-15 18:24:38 +0200

    feat: add music data

---
+++ src/app/songs.ts
@@ -0,0 +1,129 @@
+export type Song = {
+  title: string;
+  artist: string;
+  lyrics: string;
+  cover: string;
+  audio: string;
+  id: string;
+};
+
+const getData = (): Song[] => [
+  {
+    title: "Sonne",
+    artist: "Rammstein",
+    lyrics: "https://genius.com/Rammstein-sonne-lyrics",
+    audio: "/proxy/mp3/6175.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b27325d8b7c6feee0c8d6593d606",
+    id: "1",
+  },
+  {
+    title: "Нервы",
+    artist: "Нервы",
+    lyrics: "https://genius.com/Nervy-nerves-lyrics",
+    audio: "/proxy/mp3/29272.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b273b0a298d09b243db0df4bc39d",
+    id: "2",
+  },
+  {
+    title: "Не с тобой я",
+    artist: "Нервы",
+    lyrics: "https://genius.com/Nervy-im-not-with-you-lyrics",
+    audio: "/proxy/mp3/328384.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b2737fa8475de0655bb66ab1b225",
+    id: "3",
+  },
+];
+
+export default getData;
+
+export const musicList: Song[] = [
+  {
+    title: "Sonne",
+    artist: "Rammstein",
+    lyrics: "https://genius.com/Rammstein-sonne-lyrics",
+    audio: "/proxy/mp3/6175.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b27325d8b7c6feee0c8d6593d606",
+    id: "1",
+  },
+  {
+    title: "194 Länder",
+    artist: "Mark Forster",
+    lyrics: "https://genius.com/Mark-forster-194-lagen-lyrics",
+    audio: "/proxy/mp3/6176.mp3",
+    cover:
+      "https://t2.genius.com/unsafe/315x315/https%3A%2F%2Fimages.genius.com%2F765b18edc71abe1ba7e057cd363ffd90.1000x1000x1.jpg",
+    id: "2",
+  },
+  {
+    title: "Dynamit",
+    artist: "Kollegah",
+    lyrics: "https://genius.com/Kollegah-and-farid-bang-dynamit-lyrics",
+    audio: "/proxy/mp3/6177.mp3",
+    cover:
+      "https://t2.genius.com/unsafe/315x315/https%3A%2F%2Fimages.genius.com%2F2f62d5c268e6559b9edeaa669106acbf.1000x1000x1.png",
+    id: "3",
+  },
+  {
+    title: "Einmal um die Welt",
+    artist: "Cro",
+    lyrics: "https://genius.com/Cro-einmal-um-die-welt-lyrics",
+    audio: "/proxy/mp3/6178.mp3",
+    cover:
+      "https://t2.genius.com/unsafe/315x315/https%3A%2F%2Fimages.genius.com%2F66f702b435599b931c62834678690da6.1000x1000x1.png",
+    id: "4",
+  },
+  {
+    title: "Erfolg ist kein Glück",
+    artist: "Kontra K",
+    lyrics: "https://genius.com/Kontra-k-erfolg-ist-kein-gluck-lyrics",
+    audio: "/proxy/mp3/6179.mp3",
+    cover:
+      "https://t2.genius.com/unsafe/315x315/https%3A%2F%2Fimages.genius.com%2F7297361945110d44d4251401d33cf95d.1000x1000x1.png",
+    id: "5",
+  },
+  {
+    title: "Hier kommt Alex",
+    artist: "Die Toten Hosen",
+    lyrics: "https://genius.com/Die-toten-hosen-hier-kommt-alex-lyrics",
+    audio: "/proxy/mp3/6180.mp3",
+    cover:
+      "https://t2.genius.com/unsafe/315x315/https%3A%2F%2Fimages.genius.com%2Fe752ca7ee39ee5f2c9747e6b7bded6ea.300x262x1.jpg",
+    id: "6",
+  },
+  {
+    title: "Deutschland",
+    artist: "Rammstein",
+    lyrics: "https://genius.com/Rammstein-deutschland-lyrics",
+    audio: "/proxy/mp3/6181.mp3",
+    cover:
+      "https://t2.genius.com/unsafe/315x315/https%3A%2F%2Fimages.genius.com%2F0c53e3088fa1b572682ddde8fcee6072.1000x1000x1.png",
+    id: "7",
+  },
+  {
+    title: "Rosen",
+    artist: "Ellen Allien",
+    lyrics: "https://genius.com/Ellen-allien-rosen-lyrics",
+    audio: "/proxy/mp3/6182.mp3",
+    cover:
+      "https://t2.genius.com/unsafe/315x315/https%3A%2F%2Fimages.genius.com%2Fd3f641aac264e46aa08e9e68f50a8d21.1000x1000x1.png",
+    id: "8",
+  },
+  {
+    title: "Ode an die Freude",
+    artist: "Friedrich Schiller",
+    lyrics: "https://genius.com/Friedrich-schiller-ode-an-die-freude-annotated",
+    audio: "/proxy/mp3/6184.mp3",
+    cover:
+      "https://t2.genius.com/unsafe/315x315/https%3A%2F%2Fimages.genius.com%2F5ykqz9evchk50llsccd1ckjvq.439x600x1.jpg",
+    id: "9",
+  },
+  {
+    title: "Atemlos durch die Nacht",
+    artist: "Helene Fischer",
+    lyrics: "https://genius.com/Helene-fischer-atemlos-durch-die-nacht-lyrics",
+    audio: "/proxy/mp3/6185.mp3",
+    cover:
+      "https://t2.genius.com/unsafe/315x315/https%3A%2F%2Fimages.genius.com%2Ff5f3db5d8a89084e59a6d5006db8eb0d.1000x1000x1.png",
+    id: "10",
+  },
+];
