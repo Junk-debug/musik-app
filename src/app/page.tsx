1a8a1bae61946e3d5b86e858e47386af8cc69732
Junk-debug <maks.naumovich@gmail.com>
2024-10-15 18:32:01 +0200

    feat: add button, add react player component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,71 +1,42 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Music } from "lucide-react";
+import Player from "@/components/player/player";
 import { useState } from "react";
+import getSongs from "./songs";
 
-const translate = async (text: string) => {
-  console.log(text);
-
-  const res = await fetch("/api/translate", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ text, targetLang: "pl" }),
-  });
-
-  const data = await res.json();
-
-  return data.translatedText;
-};
-
-const searchSong = async (search: string) => {
-  const res = await fetch(`/api/search-lyrics?query=${search}`);
-  const data = await res.json();
-  return data.hits;
-};
-
-const getLyrics = async (url: string) => {
-  const res = await fetch(`/api/lyrics/${url}`);
-  const data = await res.json();
-  return data.lyrics;
-};
-
-const searchLyrics = async (search: string) => {
-  const searchRes = await searchSong(search);
-  console.log(searchRes[0].result);
-  const url = searchRes[0].result.url;
-  const lyrics = await getLyrics(url);
-
-  console.log(lyrics);
-
-  const translatedLyrics = await translate(lyrics);
-
-  return { lyrics, translatedLyrics };
-};
+const Logo = () => (
+  <h1 className="text-2xl font-gilroy font-extrabold">Musik app</h1>
+);
 
 export default function Home() {
-  const [search, setSearch] = useState("");
-  const [lyrics, setLyrics] = useState("");
+  const [songs] = useState(getSongs());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Type song name</h1>
-      <div>
-        <input
-          type="text"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-        <button
-          onClick={async () => {
-            const { lyrics, translatedLyrics } = await searchLyrics(search);
-            setLyrics(lyrics + "\n\n\n\n\n" + translatedLyrics);
+    <div className="min-h-screen px-4 py-8 mx-auto max-w-4xl flex flex-col">
+      <header className="flex items-center justify-between">
+        <Logo />
+        <Button>
+          <Music size={20} className="mr-1" /> Bibliothek
+        </Button>
+      </header>
+      <main className="flex-grow flex flex-col  justify-center">
+        <Player
+          onNext={() => {
+            console.log(currentSong);
+            setCurrentSong(songs[songs.indexOf(currentSong) + 1] || songs[0]);
           }}
-        >
-          Search
-        </button>
-      </div>
-      <pre>{lyrics}</pre>
+          onPrev={() =>
+            setCurrentSong(
+              songs[songs.indexOf(currentSong) - 1] || songs[songs.length - 1]
+            )
+          }
+          currentSong={currentSong}
+        />
+      </main>
+      <footer>footer</footer>
     </div>
   );
 }
---
+++ src/components/player/player.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import {
+  FastForward,
+  Pause,
+  Play,
+  Repeat,
+  Rewind,
+  SkipBack,
+  SkipForward,
+  Volume2,
+  VolumeX,
+} from "lucide-react";
+import AudioPlayer, { RHAP_UI } from "react-h5-audio-player";
+import "./player.css";
+import { Song } from "@/app/songs";
+
+import Image from "next/image";
+
+type Props = {
+  currentSong: Song;
+  onNext: () => void;
+  onPrev: () => void;
+};
+
+const customIcons = {
+  play: <Play className="w-full h-full" />,
+  pause: <Pause className="w-full h-full" />,
+  rewind: <Rewind className="w-full h-full" />,
+  forward: <FastForward className="w-full h-full" />,
+  next: <SkipForward className="w-full h-full" />,
+  previous: <SkipBack className="w-full h-full" />,
+  volumeMute: <VolumeX className="w-full h-full" />,
+  volume: <Volume2 className="w-full h-full" />,
+  loop: <Repeat className="w-full h-full text-primary" />,
+  loopOff: <Repeat className="w-full h-full text-muted-foreground" />,
+} satisfies AudioPlayer["props"]["customIcons"];
+
+const fallbackImage =
+  "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLW11c2ljLTIiPjxjaXJjbGUgY3g9IjgiIGN5PSIxOCIgcj0iNCIvPjxwYXRoIGQ9Ik0xMiAxOFYybDcgNCIvPjwvc3ZnPg==";
+
+const Player = ({ currentSong, onNext, onPrev }: Props) => {
+  const { cover, title, artist, audio } = currentSong;
+
+  return (
+    <div className="flex flex-col items-center">
+      <Image
+        src={cover || fallbackImage}
+        alt={title}
+        width={300}
+        height={300}
+        priority
+        className="mb-8"
+      />
+      <h3 className="flex flex-col gap-2 items-center mb-2">
+        <span className="text-4xl font-gilroy font-bold">{title}</span>
+        <span className="font-gilroy">{artist}</span>
+      </h3>
+      <AudioPlayer
+        className="max-w-lg"
+        defaultDuration={null}
+        src={audio}
+        onClickNext={onNext}
+        onClickPrevious={onPrev}
+        onEnded={onNext}
+        showJumpControls={false}
+        showSkipControls
+        showDownloadProgress
+        customProgressBarSection={[
+          RHAP_UI.CURRENT_TIME,
+          RHAP_UI.PROGRESS_BAR,
+          RHAP_UI.DURATION,
+        ]}
+        customIcons={customIcons}
+        showFilledVolume
+      />
+    </div>
+  );
+};
+
+export default Player;
