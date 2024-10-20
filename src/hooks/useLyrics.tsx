557ccdaa944008a7d1a9b272aa5f1f74557891a9
Junk-debug <maks.naumovich@gmail.com>
2024-10-21 00:21:16 +0200

    fix: add abort controller to abort request on clean up function, add signal props to fetchLyrics and translate functions

--- src/hooks/useLyrics.tsx
+++ src/hooks/useLyrics.tsx
@@ -6,8 +6,16 @@ function isError(error: unknown): error is Error {
   return error instanceof Error;
 }
 
-const fetchLyrics = async (lyricsLink: string): Promise<string> => {
-  const response = await fetch(`/api/lyrics/${encodeURIComponent(lyricsLink)}`);
+const fetchLyrics = async (
+  lyricsLink: string,
+  signal?: AbortSignal
+): Promise<string> => {
+  const response = await fetch(
+    `/api/lyrics/${encodeURIComponent(lyricsLink)}`,
+    {
+      signal,
+    }
+  );
   const data = await response.json();
 
   if (!response.ok) {
@@ -17,7 +25,10 @@ const fetchLyrics = async (lyricsLink: string): Promise<string> => {
   return data.lyrics;
 };
 
-const translate = async (lyrics: string): Promise<string> => {
+const translate = async (
+  lyrics: string,
+  signal?: AbortSignal
+): Promise<string> => {
   const response = await fetch("/api/translate", {
     method: "POST",
     headers: {
@@ -27,6 +38,7 @@ const translate = async (lyrics: string): Promise<string> => {
       text: lyrics,
       targetLang: targetLanguage,
     }),
+    signal,
   });
 
   const data = await response.json();
@@ -51,7 +63,8 @@ export default function useLyrics(lyricsLink: string) {
     useState<Error | null>(null);
 
   useEffect(() => {
-    let ignore = false;
+    const controller = new AbortController();
+    const { signal } = controller;
 
     const makeRequest = async () => {
       setIsLoadingLyrics(true);
@@ -61,37 +74,53 @@ export default function useLyrics(lyricsLink: string) {
       setTranslatedLyricsError(null);
 
       try {
-        const lyrics = await fetchLyrics(lyricsLink);
+        const lyrics = await fetchLyrics(lyricsLink, signal);
 
-        if (!ignore) {
-          console.log("set lyrics", lyrics.slice(0, lyrics.indexOf("\n")));
-          setLyrics(lyrics);
+        setLyrics(lyrics);
+
+        if (!lyrics) {
+          return;
         }
 
-        if (lyrics) {
-          try {
-            const translatedLyrics = await translate(lyrics);
+        try {
+          const translatedLyrics = await translate(lyrics, signal);
 
-            if (!ignore) {
-              setTranslatedLyrics(translatedLyrics);
-            }
-          } catch (error) {
-            console.error("Error translating lyrics:", error);
-
-            if (isError(error)) {
-              setTranslatedLyricsError(error);
-            }
-          } finally {
-            setIsLoadingTranslatedLyrics(false);
+          setTranslatedLyrics(translatedLyrics);
+        } catch (error) {
+          console.log(signal.aborted);
+          if (signal.aborted) {
+            console.log("translate request aborted: ", signal.reason);
+            return;
           }
+
+          console.error("Error translating lyrics:", error);
+
+          if (isError(error)) {
+            setTranslatedLyricsError(error);
+          }
+        } finally {
+          if (signal.aborted) {
+            return;
+          }
+
+          setIsLoadingTranslatedLyrics(false);
         }
       } catch (error) {
+        if (signal.aborted) {
+          console.log("lyrics request aborted: ", signal.reason);
+          return;
+        }
+
         console.error("Error fetching lyrics:", error);
 
         if (isError(error)) {
           setLyricsError(error);
         }
       } finally {
+        if (signal.aborted) {
+          return;
+        }
+
         setIsLoadingLyrics(false);
         setIsLoadingTranslatedLyrics(false);
       }
@@ -102,7 +131,7 @@ export default function useLyrics(lyricsLink: string) {
     }
 
     return () => {
-      ignore = true;
+      controller.abort("aborted because lyricsLink changed");
     };
   }, [lyricsLink]);
 
